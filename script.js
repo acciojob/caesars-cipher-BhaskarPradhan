07d836296d8f1b46d4364e9a8c0d108ce93c7932
BhaskarPradhan <135045424+BhaskarPradhan@users.noreply.github.com>
2023-09-29 12:11:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,12 +31,50 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+// function rot13(encodedStr) {
+//   let decodedArr = []; // Your Result goes here
+//   // Only change code below this line
 
-  return; //return decodedArr
+//   return; //return decodedArr
+// }
+
+function rot13(encodedStr) {
+  // Initialize an empty string to store the decoded result
+  let decodedStr = '';
+
+  // Loop through each character in the input string
+  for (let i = 0; i < encodedStr.length; i++) {
+    // Get the current character
+    const char = encodedStr.charAt(i);
+
+    // Check if the character is an uppercase letter (A-Z)
+    if (/[A-Z]/.test(char)) {
+      // Convert the character to its Unicode code
+      const charCode = encodedStr.charCodeAt(i);
+
+      // Check if the character is between 'A' and 'M' (the first half of the alphabet)
+      if (charCode >= 65 && charCode <= 77) {
+        // Shift the character code by 13 positions forward
+        decodedStr += String.fromCharCode(charCode + 13);
+      } else {
+        // Shift the character code by 13 positions backward
+        decodedStr += String.fromCharCode(charCode - 13);
+      }
+    } else {
+      // If the character is not an uppercase letter, simply append it to the result
+      decodedStr += char;
+    }
+  }
+
+  // Return the decoded string
+  return decodedStr;
 }
+
+// Example usage:
+const encodedString = "GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.";
+const decodedString = rot13(encodedString);
+console.log(decodedString); // Outputs: "THE QUICK BROWN FOX JUMPS OVER THE LAYR DOG."
+
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
